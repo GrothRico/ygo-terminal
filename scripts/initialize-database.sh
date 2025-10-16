213376cff786d4a3eaae689a78b4d47a6c2a3c5e
Rico Groth <rico.groth@check24.de>
2025-10-16 17:05:34 +0200

    add mongodb database and data initialization script (german text data)

---
+++ scripts/initialize-database.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+if [ ! -f "var/data.json" ]; then
+    echo "Downloading data"
+    echo "================================================"
+    curl "https://db.ygoprodeck.com/api/v7/cardinfo.php?language=de" | jq '.data' > var/data.json
+fi
+echo "Start up MongoDB container"
+echo "================================================"
+docker compose up -d
+echo "Wait for MongoDB container to become ready"
+echo "================================================"
+sleep 5
+echo "Import card data to MongoDB container"
+echo "================================================"
+cat var/data.json | docker exec -i ygo-terminal-ygo-card-db-1 mongoimport \
+  --db db --collection cards --jsonArray --uri "mongodb://root:root@127.0.0.1:27017/db?authSource=admin"
